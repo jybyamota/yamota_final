45b3fd4e0ecd5ad2e8040722955d08cb0f6977d7
Henri Dillo <HenriDillo@users.noreply.github.com>
2026-04-22 16:34:27 +0800

    Add sessions table back to migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,6 +21,15 @@ return new class extends Migration
             $table->timestamp('updated_on')->nullable();
             $table->unsignedBigInteger('updated_by')->nullable();
         });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
     }
 
     /**
@@ -28,6 +37,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('sessions');
         Schema::dropIfExists('users');
     }
 };
